972bafdcd554cb8c72425a9956326b3fa1128bba
richikaberry <berryshorthear@gmail.com>
2025-07-02 09:16:01 +0900

    ninth commit

--- app/routes/cook.cookdog.tsx
+++ app/routes/cook.cookdog.tsx
@@ -1,7 +1,23 @@
+import { ActionFunctionArgs } from "@remix-run/node"
+import { DynamicInput } from "~/components/dinamicInput"
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  // input[0]〜無限大数的に増大するinput要素の取得
+  
+}
+
 export default function CookDog() {
   return (
     <div>
-      <label>Cook Dog</label>
+      <label>Cook Dogへようこそ</label>
+      <div>
+        <label>最初は食材を入力してください</label>
+        <form method="get" >
+          <DynamicInput />
+          <button id="formButton" type="submit" >次へ</button>
+        </form>
+      </div>
     </div>
   )
 }
