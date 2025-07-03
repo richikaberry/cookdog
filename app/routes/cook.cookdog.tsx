06b9bc944845a419c966f4cca34c469878a53dd8
richikaberry <berryshorthear@gmail.com>
2025-07-03 09:26:38 +0900

    tenth commit

--- app/routes/cook.cookdog.tsx
+++ app/routes/cook.cookdog.tsx
@@ -1,10 +1,23 @@
-import { ActionFunctionArgs } from "@remix-run/node"
+import { ActionFunctionArgs, redirect } from "@remix-run/node"
+import { Form } from "@remix-run/react";
 import { DynamicInput } from "~/components/dinamicInput"
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData();
   // input[0]〜無限大数的に増大するinput要素の取得
-  
+  const formInputs = formData.getAll('input[]');
+  const inputs = String(formInputs);
+  if (typeof inputs !== "string") {
+    throw new Error("inputs be a string")
+  }
+  const errors: any = {};
+  if (inputs === "") {
+    errors.inputs = "入力は必須です";
+  }
+  if (Object.keys(errors).length > 0) {
+    return ({ errors });
+  }
+  return redirect(`/cook/seasoning?cookdog=${inputs}`);
 }
 
 export default function CookDog() {
@@ -13,10 +26,10 @@ export default function CookDog() {
       <label>Cook Dogへようこそ</label>
       <div>
         <label>最初は食材を入力してください</label>
-        <form method="get" >
+        <Form method="get" >
           <DynamicInput />
           <button id="formButton" type="submit" >次へ</button>
-        </form>
+        </Form>
       </div>
     </div>
   )
